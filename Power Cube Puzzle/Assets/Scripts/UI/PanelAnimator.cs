f545cc1c672bdedee2c76225af55a51f6cc58866
FreyHK <freykjelstroem@gmail.com>
2018-05-12 22:11:41 +0200

    Panel glitch fix
    Panels are now in selected state (open/closed) instead of transitioning when game starts

--- Power Cube Puzzle/Assets/Scripts/UI/PanelAnimator.cs
+++ Power Cube Puzzle/Assets/Scripts/UI/PanelAnimator.cs
@@ -9,6 +9,13 @@ public class PanelAnimator : MonoBehaviour {
 	[SerializeField] Button OpenButton;
 	[SerializeField] bool isOpen = false;
 
+	void Awake () {
+		if (!isOpen) {
+			anim.Play("Close", 0, 1f);
+		}else
+			anim.Play("Open", 0, 1f);
+	}
+
 	public void ToggleOpen () {
 		//Don't open/close if already animating
 		if (anim.GetCurrentAnimatorStateInfo (0).IsName ("Close -> Open") ||
