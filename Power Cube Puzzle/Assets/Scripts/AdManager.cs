37a950532f86f88c1b97e85dedfa1de248e7bf68
Frey Kjelstrøm <freykjelstroem@gmail.com>
2018-07-03 17:56:54 +0200

    Added Ads API

--- Power Cube Puzzle/Assets/Scripts/AdManager.cs
+++ Power Cube Puzzle/Assets/Scripts/AdManager.cs
@@ -1,13 +1,24 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Advertisements;
 
+
 public class AdManager : MonoBehaviour {
+
+    #if UNITY_IOS
+    private string gameId = "1735021";
+    #elif UNITY_ANDROID
+    private string gameId = "1735020";
+    #endif
+
 
     public const float AdWaitTime = 200f;
 
     void Start() {
+	if (Advertisement.isSupported) {
+            Advertisement.Initialize (gameId, true);
+        }
         cooldown = (float)PlayerPrefs.GetInt("AdCooldown", 0);
     }
 
